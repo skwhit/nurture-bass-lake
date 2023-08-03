ba2852cde52a60b641921732ffb64d760bfc1f02
skwhit <whitlowspencer@gmail.com>
2023-08-02 18:17:39 -0700

    finished carousel on homepage. started content on about page.

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import React from "react";
+import "./About.css";
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <main className="about-container">
+      <section className="restaurant-info">
+        <div className="restaurant-info-left">
+          <div>
+            {" "}
+            <h1>Mission Statement</h1>
+            <p>
+              Our mission is to help people nurture themselves and their
+              relationships with others over good food and to provide a space
+              for all to gather and feel welcomed.
+            </p>
+          </div>
+          <div>
+            <h1>Who we serve</h1>
+            <p>
+              Whether you're a Bass Lake local or you are vacationing at Bass Lake.
+              We will offer our customers a nurturing environment in which to
+              linger and enjoy their food and beverages. We will have
+              comfortable indoor dining as well as patio dining at the back of
+              the building that has a rare view of Bass Lake. The patio will be
+              a very relaxing area to enjoy casual dining under the trees.
+            </p>
+          </div>
+          <div>
+            <h1>What we serve</h1>
+            <p>
+              Nurture Bass Lake offers excellent coffee choices, and
+              various breakfast items in the Village of Bass Lake, California,
+              an exclusive resort area and fun mountain community, which is
+              located just south of Yosemite.
+            </p>
+          </div>
+        </div>
+        <div className="restaurant-info-right"></div>
+      </section>
+    </main>
+  );
 }
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,27 +1,45 @@
 import React from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import acai1 from '../../assets/acai1.jpg'
-import coffee1 from '../../assets/coffee1.jpg'
-import eggs1 from '../../assets/eggs1.jpg'
+import acai1 from "../../assets/acai1.jpg";
+import coffee1 from "../../assets/coffee1.jpg";
+import eggs1 from "../../assets/eggs1.jpg";
 
-import "./Home.css"
+import "./Home.css";
 
 export default function Home() {
   return (
-    <Carousel className="carousel">
-      <div>
-        <img src={acai1} />
-        <p className="legend">Legend 1</p>
-      </div>
-      <div>
-        <img src={coffee1} />
-        <p className="legend">Legend 2</p>
-      </div>
-      <div>
-        <img src={eggs1} />
-        <p className="legend">Legend 3</p>
-      </div>
-    </Carousel>
+    <main className="home-container">
+      <section className="carousel-container">
+        <Carousel
+          autoPlay
+          infiniteLoop
+          useKeyboardArrows
+          interval={5000}
+          showArrows={true}
+          showThumbs={false}
+          showStatus={false}
+          labels={false}
+          className="carousel"
+        >
+          <div className="carousel-one">
+            <img src={acai1} />
+            {/* <p className="legend">Legend 1</p> */}
+          </div>
+          <div className="carousel-two">
+            <img src={coffee1} />
+            {/* <p className="legend">Legend 2</p> */}
+          </div>
+          <div className="carousel-three">
+            <img src={eggs1} />
+            {/* <p className="legend">Legend 3</p> */}
+          </div>
+        </Carousel>
+      </section>
+      <section className="home-title">
+        <h1>Nurture Bass Lake</h1>
+        <h3>Healthy food for your adventures</h3>
+      </section>
+    </main>
   );
 }
