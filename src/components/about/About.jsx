126b18e7581c9f14089231e3aca1acbac88eccad
skwhit <whitlowspencer@gmail.com>
2023-08-03 16:36:39 -0700

    customized about page

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import "./About.css";
+import bassLake1 from "../../assets/bassLake1.jpg";
+import bassLake2 from "../../assets/bassLake2.jpg";
+import bassLake3 from "../../assets/bassLake3.jpg";
+import restaurant from "../../assets/restaurant-inside.jpg"
+import acai1 from "../../assets/acai1.jpg";
+import coffee1 from "../../assets/coffee1.jpg";
+import eggs1 from "../../assets/eggs1.jpg";
 
 export default function About() {
   return (
     <main className="about-container">
       <section className="restaurant-info">
-        <div className="restaurant-info-left">
+        <div className="restaurant-info-one">
           <div>
             {" "}
             <h1>Mission Statement</h1>
@@ -16,27 +23,67 @@ export default function About() {
             </p>
           </div>
           <div>
+            {" "}
+            <h1>Where we're located</h1>
+            <p>
+              We are located in the heart of the Pine's Village in Bass Lake,
+              California. Bass Lake is an exclusive resort area and fun mountain
+              community, located just south of Yosemite National Park.
+            </p>
+          </div>
+        </div>
+        <div className="restaurant-info-two">
+          <div className="restaurant-info-two-left">
+            <div>
+              <img src={bassLake1} alt="Image of Bass Lake" />
+            </div>
+            <div>
+              <img src={bassLake3} alt="Image of Bass Lake" />
+            </div>
+          </div>
+
+          <div className="restaurant-info-two-right">
+            <img src={bassLake2} alt="Image of Bass Lake" />
+          </div>
+        </div>
+        <div className="restaurant-info-three">
+          <div className="restaurant-info-three-top">
+            <img src={restaurant} alt="Image inside the restaurant" />
+          </div>
+          <div className="restaurant-info-three-bottom">
+            <div>
+              <img src={acai1} alt="Image of Acai bowl menu item" />
+            </div>
+            <div>
+              <img src={coffee1} alt="Image of coffee menu item" />
+            </div>
+            <div>
+              <img src={eggs1} alt="Image of breakfast menu item" />
+            </div>
+          </div>
+        </div>
+
+        <div className="restaurant-info-four">
+          <div>
             <h1>Who we serve</h1>
             <p>
-              Whether you're a Bass Lake local or you are vacationing at Bass Lake.
-              We will offer our customers a nurturing environment in which to
-              linger and enjoy their food and beverages. We will have
-              comfortable indoor dining as well as patio dining at the back of
-              the building that has a rare view of Bass Lake. The patio will be
-              a very relaxing area to enjoy casual dining under the trees.
+              Whether you're a Bass Lake local or you are vacationing at Bass
+              Lake, we have a place for you. We offer our customers a inviting
+              environment in which to linger and enjoy food and beverages. We
+              have comfortable indoor dining as well as patio dining at the back
+              of the building that has a rare view of Bass Lake. The patio is a
+              very relaxing area to enjoy casual dining under the trees.
             </p>
           </div>
           <div>
             <h1>What we serve</h1>
             <p>
-              Nurture Bass Lake offers excellent coffee choices, and
-              various breakfast items in the Village of Bass Lake, California,
-              an exclusive resort area and fun mountain community, which is
-              located just south of Yosemite.
+              Nurture Bass Lake offers excellent coffee choices, and various
+              breakfast items. We also specialize in Acai bowls made with fresh
+              and locally sourced ingredients.
             </p>
           </div>
         </div>
-        <div className="restaurant-info-right"></div>
       </section>
     </main>
   );
